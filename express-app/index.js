247435e2037f75b4a96c8d69a9448668f6b5532f
seanchok <seanchok@gmail.com>
2019-09-11 18:17:28 +0800

    add mongoose and connect to mongodb atlas cluster

--- express-app/index.js
+++ express-app/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const env = require('dotenv');
 env.config();
+
+// connect to mongoose
+const mongo_uri = process.env.MONGO_URI;
+mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(
+  () => {
+    console.log("Mongoose connection successful");
+  },
+  err => {
+    console.log("Mongoose connection error");
+    console.error(err);
+  }
+);
+
 
 const rootRoutes = require('./routes/root');
 
