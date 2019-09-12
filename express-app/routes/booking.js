a0c19c59e01fbe22cc36bedd887ce491443185fe
seanchok <seanchok@gmail.com>
2019-09-13 00:34:57 +0800

    create basic api key authentication
    #4

--- express-app/routes/booking.js
+++ express-app/routes/booking.js
@@ -14,11 +14,17 @@ const handleError = require('./errorHandler');
 
 // routes
 router.post('/create', async (req, res, next) => {
-  const newBooking = req.body;
-  let error, savedBooking;
-  [error, savedBooking] = await to(Booking.create(newBooking));
-  if (error) return handleError(res, error, 'Failed to create booking');
-  res.status(201).json(savedBooking);
+  if (req.body.api_key === process.env.API_KEY) {
+    let error, savedBooking;
+    const newBooking = req.body;
+    newBooking.api_key = undefined;
+    [error, savedBooking] = await to(Booking.create(newBooking));
+    if (error) return handleError(res, error, 'Failed to create booking');
+    console.log("Inserted new booking:\n" + savedBooking);
+    res.status(201).json(savedBooking);
+    return;
+  }
+  handleError(res, 'Unauthorized access', 'Unauthorized access', 401);
 })
 
 module.exports = router;
