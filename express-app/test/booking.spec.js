c65c38f96eb2f79ef5be6192de073db164c5785b
seanchok <seanchok@gmail.com>
2019-09-15 13:04:41 +0800

    write tests for bad booking
    #9

--- express-app/test/booking.spec.js
+++ express-app/test/booking.spec.js
@@ -1,5 +1,6 @@
 const server = require('../index');
 const to = require('await-to-js').default;
+const cloneDeep = require('lodash.clonedeep');
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -12,14 +13,14 @@ const Booking = require('../models/booking');
 const Room = require('../models/room');
 
 describe('Bookings', () => {
-  let booking = {
+  const booking = {
     room: '305',
     start: new Date(2019, 9, 20, 8),
     end: new Date(2019, 9, 20, 9),
     api_key: process.env.API_KEY
   }
 
-  beforeEach((done) => {
+  before((done) => {
     Booking.deleteMany({}, (error) => {if (error) return done()});
     Room.findOne({roomName: booking.room}, (error, foundRoom) => {
       if (error) return done();
@@ -34,10 +35,7 @@ describe('Bookings', () => {
   describe('/POST create booking and add to rooms', () => {
     it('should return saved booking and saved room', (done) => {
       chai.request(server).post('/booking/create').send(booking).end((error, res) => {
-        if (error) {
-          handleError(res, error, 'Failed to create booking');
-          done();
-        }
+        if (error) return done(error);
         res.should.have.status(201);
         res.body.should.have.keys('savedBooking', 'savedRoom');
         const savedBooking = res.body.savedBooking, savedRoom = res.body.savedRoom;
@@ -48,4 +46,23 @@ describe('Bookings', () => {
       })
     })
   })
+
+  describe('/POST create invalid booking', () => {
+    itTestsBadBooking(booking, 'room', 'invalid room');
+    itTestsBadBooking(booking, 'start', 'not a date object');
+    itTestsBadBooking(booking, 'end', null);
+  })
 })
+
+function itTestsBadBooking(booking, key, value) {
+  let badBooking = cloneDeep(booking);
+  badBooking[key] = value;
+  it('should return ' + key + ' error', (done) => {
+    chai.request(server).post('/booking/create').send(badBooking).end((error, res) => {
+      if (error) return done(error);
+      res.should.have.status(400);
+      res.body.should.be.a('object').that.have.all.keys('error');
+      done();
+    })
+  })
+}
