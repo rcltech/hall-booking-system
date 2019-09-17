e0a000a63a40d4c318e96a1ad9059be920b95c45
Sean Chok <seanchok@gmail.com>
2019-09-17 10:17:16 +0800

    create get request handler for rooms (#14)
    * create room mongoose schema
    
    #9
    
    * create get request handler for rooms
    
    #9
    
    * write tests for getting rooms using mocha
    
    #9
    
    * write logic to link creating booking to updating corresponding room
    
    #9
    
    * rewrite tests for /POST create booking to check for updated room
    
    #9
    
    * change API endpoints, and push-heroku instructions in readme
    
    #9
    
    * rewrite mocha tests for removing booking and room hoursBooked element before testing create booking route
    
    #9
    
    * write comprehensive logic to link booking creation with room updates
    
    #9
    
    * write tests for bad booking
    
    #9
    
    * add booking validation for checking before creating new booking and updating room
    
    #9
    
    * add access control to room get request handler
    
    #9
    
    * fix type in access control
    
    #9

--- express-app/models/booking.js
+++ express-app/models/booking.js
@@ -1,14 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const moment = require('moment');
+
+const listOfRooms = ['204', '305'];
 
 const bookingSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  room: String,
-  start: Date,
-  end: Date
+  room: {
+    type: String,
+    enum: listOfRooms,
+    required: [true, 'No room provided']
+  },
+  start: {
+    type: Date,
+    required: [true, 'No start datetime provided']
+  },
+  end: {
+    type: Date,
+    required: [true, 'No end datetime provided']
+  }
+})
+
+bookingSchema.pre('init', function(booking) {
+  booking.start = moment(booking.start).format();
+  booking.end = moment(booking.end).format();
+  return booking;
 })
 
 const Booking = mongoose.model('Booking', bookingSchema);
--- express-app/routes/booking.js
+++ express-app/routes/booking.js
@@ -8,20 +8,47 @@ router.use(bodyParser.json());
 
 // schema files
 const Booking = require('../models/booking');
+const Room = require('../models/room');
 
 // error handler
 const handleError = require('./errorHandler');
 
+// create booking
+const createBooking = async(res, booking) => {
+  let error, savedBooking;
+  [error, savedBooking] = await to(Booking.create(booking));
+  if (error) return handleError(res, error, 'Failed to create booking');
+  return savedBooking;
+}
+
+// logic to link creating booking to updating room
+const updateRoom = async (res, booking) => {
+  let error, foundRoom, savedRoom;
+  const query = { roomName: booking.room };
+  [error, foundRoom] = await to(Room.findOne(query));
+  if (error) return handleError(res, error, 'Failed to find a room with name: ' + query.roomName);
+
+  error = foundRoom.updateHoursBooked(booking.start, booking.end);
+  if (error) return handleError(res, error, 'Failed to add hours booked');
+
+  [error, savedRoom] = await to(Room.findOneAndUpdate(query, foundRoom, {new: true, useFindAndModify: false}));
+  if (error) return handleError(res, error, 'Failed to update the room');
+
+  return savedRoom;
+}
+
 // routes
 router.post('/create', async (req, res, next) => {
   if (req.body.api_key === process.env.API_KEY) {
-    let error, savedBooking;
-    const newBooking = req.body;
-    newBooking.api_key = undefined;
-    [error, savedBooking] = await to(Booking.create(newBooking));
-    if (error) return handleError(res, error, 'Failed to create booking');
-    console.log("Inserted new booking:\n" + savedBooking);
-    res.status(201).json(savedBooking);
+    const booking = req.body;
+    booking.api_key = undefined;
+    let newBooking = new Booking(booking);
+    let bookingError = await newBooking.validateSync();
+    if (bookingError) return handleError(res, 'Invalid booking', 'Invalid booking', 400);
+    let savedRoom = await updateRoom(res, booking);
+    if (savedRoom.error) return;
+    let savedBooking = await createBooking(res, booking);
+    res.status(201).json({savedBooking, savedRoom});
     return;
   }
   handleError(res, 'Unauthorized access', 'Unauthorized access', 401);
