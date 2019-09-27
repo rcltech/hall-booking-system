7abab8be1bcddca79c9ad6c98917789d0243ce19
seanchok <seanchok@gmail.com>
2019-09-27 15:40:48 +0800

    create user auth for dummy user
    #21

--- express-app/middleware/authUser.js
+++ express-app/middleware/authUser.js
@@ -2,12 +2,19 @@ const jwt = require('jsonwebtoken');
 const to = require('await-to-js').default;
 const handleError = require('../routes/errorHandler');
 const key = process.env.API_KEY;
+const dummyUserId = process.env.DUMMY_USER_ID;
 
 const authUser = async (req, res, next) => {
   // temporary jwt signature to verify payload
   const token = req.body.token;
   jwt.verify(token, key, (error, payload) => {
-    if (error) return handleError(res, error, 'Failed to auth user', 401);
+    if (error || dummyUserId !== payload.userId)
+      return handleError(
+        res,
+        error ? error : 'Invalid user',
+        'Failed to auth user',
+        401
+      );
     req.body = payload;
   });
   next();
