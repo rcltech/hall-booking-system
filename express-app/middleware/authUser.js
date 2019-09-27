62168b787c0af5143f0e10e89c9434be82899a7a
seanchok <seanchok@gmail.com>
2019-09-27 15:40:48 +0800

    create user auth for dummy user

--- express-app/middleware/authUser.js
+++ express-app/middleware/authUser.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const handleError = require('../routes/errorHandler');
 const key = process.env.API_KEY;
+const dummyUserId = process.env.DUMMY_USER_ID;
 
 const authUser = (req, res, next) => {
   let token = req.headers['authorization'];
