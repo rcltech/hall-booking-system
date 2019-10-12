31852f79e282f381a26010cf72d6abd4c129be86
seanchok <seanchok@gmail.com>
2019-10-13 01:24:37 +0800

    create apollo context in react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './components/main/Homepage';
 import ChooseRoom from './components/main/ChooseRoom';
 import ChooseDate from './components/main/ChooseDate';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/room" component={ChooseRoom} />
-            <Route path="/date" component={ChooseDate} />
-          </Switch>
-        </div>
-      </Router>
-    );
+const GET_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      email
+      image_url
+      phone
+      first_name
+      room_no
+    }
   }
-}
+`;
+
+const App = () => {
+  const { data, loading, error } = useQuery(GET_USER);
+  if (loading || !data) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/room" component={ChooseRoom} />
+          <Route path="/date" component={ChooseDate} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
