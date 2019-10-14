c62077c29458db0e8d9e4e025ec4437408464170
Sean Chok <seanchok@gmail.com>
2019-10-14 13:40:29 +0800

    create apollo context in react app (#36)
    * create apollo context in react
    
    * omit apollo branch from heroku deploys in travis
    
    * switch to querying me in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './components/main/Homepage';
 import ChooseRoom from './components/main/ChooseRoom';
 import ChooseDate from './components/main/ChooseDate';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/room" component={ChooseRoom} />
-            <Route path="/date" component={ChooseDate} />
-          </Switch>
-        </div>
-      </Router>
-    );
+const GET_ME = gql`
+  query me {
+    me {
+      username
+      image_url
+      first_name
+      last_name
+      room_no
+    }
   }
-}
+`;
+
+const App = () => {
+  const { data, loading, error } = useQuery(GET_ME);
+  if (loading || !data) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/room" component={ChooseRoom} />
+          <Route path="/date" component={ChooseDate} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
