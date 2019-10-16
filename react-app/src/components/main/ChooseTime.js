f21c27b08ebcbe5b8b17ddf7cc0784d0ef0a6c42
Welvin <wewe2712@connect.hku.hk>
2019-10-16 17:29:08 +0800

    Add timepicker and time validation functionality

--- react-app/src/components/main/ChooseTime.js
+++ react-app/src/components/main/ChooseTime.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Timetable from 'react-timetable-events';
-import NavBar from '../complement/NavBar.js';
+import NavBar from '../complement/NavBar';
+import Timepicker from '../complement/Timepicker';
 const moment = require('moment');
 const getRooms = require('../../functions/getRooms');
+const validateTime = require('../../functions/validateTime');
 
 const style = {
   container: {
@@ -15,11 +17,17 @@ export default class ChooseTime extends Component {
     const {
       state: { room, date }
     } = props.location;
+    let start = moment(date);
+    let end = moment(date);
+    start.set({ hour: 12 });
+    end.set({ hour: 13 });
     super(props);
     this.state = {
       events: undefined,
       room,
-      date
+      date,
+      start,
+      end
     };
   }
 
@@ -30,16 +38,27 @@ export default class ChooseTime extends Component {
     });
   };
 
+  onContinue = async (start, end) => {
+    const { events } = this.state;
+    const timeslots = events[Object.keys(events)[0]];
+    if (validateTime(timeslots, start, end) === true) {
+      alert('TODO : Send post request and redirect to confirmation page');
+      //send a post request to store new booking
+      //redirect to booking confirmation page
+    }
+  };
+
   render() {
-    let { events, room, date } = this.state;
-    date = moment(date).format('DD-MM-YYYY');
+    const { events, room, date, start, end } = this.state;
+    let dateString = moment(date).format('LL');
     return (
       <div style={style.container}>
         <NavBar backPath="/room" />
         <p>
           What is the most suitable timeslot for you? <br /> Room : {room}{' '}
-          <br /> Date : {date}
+          <br /> Date : {dateString}
         </p>
+        <Timepicker start={start} end={end} onContinue={this.onContinue} />
         {events ? <Timetable events={events} /> : <div></div>}
       </div>
     );
