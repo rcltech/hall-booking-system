47a17a20d23198bc5bc758001f39d64cca47bcb5
Welvin <wewe2712@connect.hku.hk>
2019-10-18 02:00:29 +0800

    Enable post request to /api/booking/create and Add modals

---
+++ react-app/src/functions/postBooking.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const moment = require('moment');
+
+const postBooking = async (room, date, startNum, endNum) => {
+  const url = 'http://rctech-owl-dev.herokuapp.com/api/booking/create';
+  const userId = '123';
+  const start = moment(date);
+  const end = moment(date);
+  start.set({
+    hours: startNum,
+    minutes: 0,
+    seconds: 0
+  });
+  end.set({
+    hours: endNum,
+    minutes: 0,
+    seconds: 0
+  });
+  const createdAt = new Date();
+  const booking = {
+    userId,
+    room,
+    start,
+    end,
+    createdAt
+  };
+  const req = {
+    booking
+  };
+  const res = await axios.post(url, req);
+  return res.statusText === 'Created';
+};
+
+module.exports = postBooking;
