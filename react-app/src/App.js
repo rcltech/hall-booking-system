78557f993c1bf4fb250004ef1f2defecd0d50876
seanchok <seanchok@gmail.com>
2019-11-12 18:18:53 +0800

    configure for local dev

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,14 +12,14 @@ import ChooseTime from './components/main/ChooseTime';
 import BookingSummary from './components/main/BookingSummary';
 
 const App = () => {
-  if (!window.location.host.includes('localhost')) {
-    localStorage.setItem('id', qs.parse(window.location.search).id || '');
-    if (localStorage.getItem('id') === '') {
-      const app_url = 'owl.rctech.club';
-      window.location.replace(
-        `https://ladybird.rctech.club/?redirectTo=${app_url}`
-      );
+  localStorage.setItem('id', qs.parse(window.location.search).id || '');
+  if (localStorage.getItem('id') === '') {
+    const app_url = 'owl.rctech.club';
+    let url = `https://ladybird.rctech.club/?redirectTo=${app_url}`;
+    if (process.env.NODE_ENV === 'development') {
+      url = `http://localhost:3000/?redirectTo=localhost:3001`;
     }
+    window.location.replace(url);
   }
 
   const ME = gql`
@@ -35,7 +35,10 @@ const App = () => {
   `;
   const { loading, error, data } = useQuery(ME);
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error</p>;
+  }
   console.log(data);
 
   return (
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -9,14 +9,15 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 
 let authorization = localStorage.getItem('id');
-if (window.location.host.includes('localhost')) {
-  authorization =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdF9uYW1lIjoiU2VhbiIsImltYWdlX3VybCI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hLS9BQXVFN21CM1BnQ0xubGstSUlCcEZwNV9ISUtKeHhqSFN5ZWJ5Tnc0OHBoNXNRPXM5Ni1jIiwicm9vbV9ubyI6IjkxNUIiLCJlbWFpbCI6InNlYW5jaG9rQGNvbm5lY3QuaGt1LmhrIiwidXNlcm5hbWUiOiI5OHNlYW45OCIsImlkIjoiY2sxcW5yaWhxNHZ1bjA3NzRrMDYyanJjaCIsImxhc3RfbmFtZSI6IkNob2siLCJwaG9uZSI6Iis4NTI2NzM0ODEzNSIsImlhdCI6MTU3MzE0OTAyNn0.ZsPeMkDtIVcNtXEw41V8fJPe3ORbSh7V-XX9dlIXFOg';
-}
+
+const uri =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/graphql'
+    : 'https://phoenix.rctech.club/graphql';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'https://phoenix.rctech.club/graphql',
+  uri,
   headers: {
     authorization
   }
