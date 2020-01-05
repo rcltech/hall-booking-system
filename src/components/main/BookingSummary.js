fa0a427a2395ae89d6add06d6bbbcc6f9107e926
utkarsh867 <utkarsh867@gmail.com>
2020-01-05 20:15:24 +0800

    #57: Makes fixes in the UI for the summary page

--- src/components/main/BookingSummary.js
+++ src/components/main/BookingSummary.js
@@ -6,7 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import RoomIcon from '@material-ui/icons/Room';
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import { makeStyles, Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import success from '../../images/modals/success.png';
 import fail from '../../images/modals/fail.png';
 import { Redirect } from 'react-router-dom';
@@ -14,20 +14,32 @@ import NavBar from '../complement/NavBar';
 import Modals from '../complement/Modals';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_BOOKING, ROOM_BOOKINGS } from '../../gql/bookings';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const moment = require('moment');
 
 const useStyles = makeStyles(theme => ({
   container: {
-    textAlign: 'center'
+    textAlign: 'center',
+    height: '100vh',
+    backgroundColor: theme.palette.primary.light
   },
   list: {
     backgroundColor: 'white'
   },
   buttonContainer: {
     display: 'flex',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
     justifyContent: 'center',
-    margin: '20px 0'
+    padding: '20px',
+    width: '100%'
+  },
+  summaryPaper: {
+    margin: 20,
+    borderRadius: '20px'
   }
 }));
 
@@ -73,7 +85,7 @@ const BookingSummary = ({
   };
 
   const renderRedirect = () => {
-    return redirect ? <Redirect to="/" /> : <div></div>;
+    return redirect ? <Redirect to="/" /> : <></>;
   };
 
   return (
@@ -86,28 +98,33 @@ const BookingSummary = ({
         }}
       />
       <NavBar backPath="/room" />
-      <List className={classes.list}>
-        <ListItem>
-          <ListItemIcon>
-            <RoomIcon />
-          </ListItemIcon>
-          <ListItemText>{room}</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
-            <EventAvailableIcon />
-          </ListItemIcon>
-          <ListItemText>{moment(date).format('LL')}</ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
-            <AccessTimeIcon />
-          </ListItemIcon>
-          <ListItemText>
-            {start} - {end}
-          </ListItemText>
-        </ListItem>
-      </List>
+      <Card elevation={3} className={classes.summaryPaper}>
+        <CardContent>
+          <List className={classes.list}>
+            <ListItem>
+              <ListItemIcon>
+                <RoomIcon color={'primary'} />
+              </ListItemIcon>
+              <ListItemText>{room}</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <EventAvailableIcon color={'primary'} />
+              </ListItemIcon>
+              <ListItemText>{moment(date).format('LL')}</ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <AccessTimeIcon color={'primary'} />
+              </ListItemIcon>
+              <ListItemText>
+                {start} - {end}
+              </ListItemText>
+            </ListItem>
+          </List>
+        </CardContent>
+      </Card>
+
       <Button
         className={classes.buttonContainer}
         color="primary"
