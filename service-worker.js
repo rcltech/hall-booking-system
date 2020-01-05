3f121facd15a3bcc4ec3695025fde67c282aad8e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-05 11:24:12 +0000

    Deploy rcltech/owl to github.com/rcltech/owl.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f28a8fa3cac0d5cdd51f77c424b4fd41.js"
+  "/precache-manifest.df0f56b5490bca8e42f3e69482a5739e.js"
 );
 
 self.addEventListener('message', (event) => {
