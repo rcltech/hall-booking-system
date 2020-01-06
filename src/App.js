6e18db69a54c9f30b854a1e272e7702b5c92fad3
Utkarsh Goel <utkarsh867@users.noreply.github.com>
2020-01-06 11:45:42 +0800

    Fixes the ME query error on login (#69)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import qs from 'query-string';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage from './components/main/Homepage';
 import ChooseRoom from './components/main/ChooseRoom';
 import ChooseDate from './components/main/ChooseDate';
@@ -9,26 +8,6 @@ import ChooseTime from './components/main/ChooseTime';
 import BookingSummary from './components/main/BookingSummary';
 
 const App = () => {
-  const isIdEmpty = () => {
-    const id = localStorage.getItem('id');
-    return id === '' || id === null || id === undefined;
-  };
-
-  // make sure that id gets stored correctly first before running anything else
-  if (isIdEmpty()) {
-    localStorage.setItem('id', qs.parse(window.location.search).id || '');
-  }
-
-  if (isIdEmpty()) {
-    const app_url = 'owl.rctech.club';
-    let url = `https://ladybird.rctech.club/?redirectTo=${app_url}`;
-    if (process.env.NODE_ENV === 'development') {
-      // local ladybird is hosted at port 3001, owl is hosted at 3000
-      url = `http://localhost:3001/?redirectTo=localhost:3000`;
-    }
-    window.location.replace(url);
-  }
-
   return (
     <Router>
       <div>
